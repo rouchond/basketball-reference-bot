0bc19f1b806a50f305ab39d73ca0be5180552211
David Rouchon <rouchon576@gmail.com>
2023-08-30 01:31:25 -0400

    Added convert_name function and aio http to pts_pg function
    last commit was bugged

--- cogs/bbrefcommands.py
+++ cogs/bbrefcommands.py
@@ -1,9 +1,12 @@
+"""
+WHEN USING VSCODE ON WINDOWS, MAKE SURE THAT THE MICROSOFT STORE PYTHON ENVIRONMENT IS SELECTED!!!
+"""
+
 import discord
 from discord.ext import commands
 from discord import app_commands
 
 import requests
-import bs4
 from bs4 import BeautifulSoup
 
 import asyncio
@@ -17,15 +20,29 @@ class BbrefCommands(commands.Cog):
     async def on_ready(self):
         print("bbrefcommands.py ready")
 
-    @app_commands.command(name="ppg", description="Returns a player's ppg")
-    async def pts_pg(self, interaction: discord.Interaction, plr: str, szn: str):
+    def convert_name(plr, draft_order):
+        if len(draft_order) == 1:
+            draft_order = "0" + draft_order
         plr_li = plr.split()
-        plr_str = plr_li[1].lower() + plr_li[0][0:2].lower() + "01.html"
-        bbr = requests.get(f"https://basketball-reference.com/players/{plr_li[1][0]}/{plr_str}").text
+        if len(plr_li[1]) > 5:
+            plr_str = plr_li[1][0:5].lower() + plr_li[0][0:2].lower() + draft_order + ".html"
+        else:
+             plr_str = plr_li[1].lower() + plr_li[0][0:2].lower() + draft_order + ".html"
+        return plr_str
+    
+    async def recent_season(soup):
+        pass
+
+    @app_commands.command(name="ppg", description="Returns a player's ppg")
+    async def pts_pg(self, interaction: discord.Interaction, player: str, draft_order: str, season="null"):
+        plr_str = self.convert_name(player, draft_order)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://basketball-reference.com/players/{plr_str[0]}/{plr_str}") as r:
+                bbr = await r.text()
         soup2 = BeautifulSoup(bbr, "lxml")
-        avgs_szn = soup2.find("tr", id=f"per_game.{szn.split('-')[1]}")
+        avgs_szn = soup2.find("tr", id=f"per_game.{season.split('-')[1]}")
         ppg = avgs_szn.find("td", {"data-stat": "pts_per_g"}).text
-        #await interaction.response.send_message(f"{' '.join(plr.split()).title()} averaged {ppg} ppg in the {szn} season.")
+        await interaction.response.send_message(f"{' '.join(plr.split()).title()} averaged {ppg} ppg in the {szn} season.")
 
         
 
