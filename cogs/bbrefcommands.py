da945978b45fae15fdbdfdcdcb10cdd78bbf331d
David Rouchon <rouchon576@gmail.com>
2023-08-31 09:56:38 -0400

    Added async functionality
    Application not responding error in discord

--- cogs/bbrefcommands.py
+++ cogs/bbrefcommands.py
@@ -20,7 +20,7 @@ class BbrefCommands(commands.Cog):
     async def on_ready(self):
         print("bbrefcommands.py ready")
 
-    def convert_name(plr, draft_order):
+    def convert_name(self,plr, draft_order):
         if len(draft_order) == 1:
             draft_order = "0" + draft_order
         plr_li = plr.split()
@@ -30,19 +30,38 @@ class BbrefCommands(commands.Cog):
              plr_str = plr_li[1].lower() + plr_li[0][0:2].lower() + draft_order + ".html"
         return plr_str
     
-    async def recent_season(soup):
-        pass
+    async def get_page(self,session, url):
+        async with session.get(url) as r:
+            return await r.text()
+
+    async def page_tasks(self,session, url):
+        task = await asyncio.create_task(self.get_page(session, url))
+        return task
+
+    def parse(self,bbr):
+        soup = BeautifulSoup(bbr, "lxml")
+        if self.season == "null":
+            avgs_table = soup.find("div", id="all_per_game-playoffs_per_game")
+            avgs_szn = avgs_table.find("tfoot")
+            ppg = avgs_szn.find("td", {"data-stat": "pts_per_g"}).text
+            return f"{self.player.title()} averaged {ppg} ppg over his career."
+        else:
+            avgs_szn = soup.find("tr", id=f"per_game.{self.season.split('-')[1]}")
+            ppg = avgs_szn.find("td", {"data-stat": "pts_per_g"}).text
+            return f"{self.player.title()} averaged {ppg} ppg in the {self.season} season."
 
     @app_commands.command(name="ppg", description="Returns a player's ppg")
-    async def pts_pg(self, interaction: discord.Interaction, player: str, draft_order: str, season="null"):
-        plr_str = self.convert_name(player, draft_order)
+    async def pts_pg(self, interaction: discord.Interaction, player: str, draft_order: str, season : str="null"):
+        self.player = player
+        self.draft_order = draft_order
+        self.season = season
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://basketball-reference.com/players/{plr_str[0]}/{plr_str}") as r:
-                bbr = await r.text()
-        soup2 = BeautifulSoup(bbr, "lxml")
-        avgs_szn = soup2.find("tr", id=f"per_game.{season.split('-')[1]}")
-        ppg = avgs_szn.find("td", {"data-stat": "pts_per_g"}).text
-        await interaction.response.send_message(f"{' '.join(plr.split()).title()} averaged {ppg} ppg in the {szn} season.")
+            plr_str = self.convert_name(self.player, self.draft_order)
+            data = await self.page_tasks(session, f"https://basketball-reference.com/players/{plr_str[0]}/{plr_str}")
+        msg = self.parse(data)
+        print(msg)
+        await interaction.response.send_message(msg)
+       
 
         
 
