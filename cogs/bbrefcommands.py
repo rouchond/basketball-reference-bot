91c06a753ae11ca9ef17a9ffe76b957dcbbeb793
David Rouchon <rouchon576@gmail.com>
2023-09-01 11:29:04 -0400

    installed lxml which fixed beautifulsoup object

--- cogs/bbrefcommands.py
+++ cogs/bbrefcommands.py
@@ -6,12 +6,10 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-import requests
 from bs4 import BeautifulSoup
 
 import asyncio
 import aiohttp
-import time
 
 class BbrefCommands(commands.Cog):
     def __init__(self, client):
@@ -24,7 +22,7 @@ class BbrefCommands(commands.Cog):
     def convert_name(self,plr, draft_order):
         if len(draft_order) == 1:
             draft_order = "0" + draft_order
-        plr_li = plr.split('_')
+        plr_li = plr.split(' ')
         if len(plr_li[1]) > 5:
             plr_str = plr_li[1][0:5].lower() + plr_li[0][0:2].lower() + draft_order + ".html"
         else:
@@ -40,24 +38,17 @@ class BbrefCommands(commands.Cog):
         return task
 
     def parse(self,bbr):
-        start_time = time.time()
         soup = BeautifulSoup(bbr, "lxml")
-        end_time = time.time()
-        print(f"{end_time - start_time} time elapsed.")
-        player_string = "".join(self.player.split("_"))
         if self.season == "null":
             avgs_table = soup.find("div", id="all_per_game-playoffs_per_game")
             avgs_szn = avgs_table.find("tfoot")
             ppg = avgs_szn.find("td", {"data-stat": "pts_per_g"}).text
-            print(f"{self.player.title()} averaged {ppg} ppg over his career.")
             return f"{self.player.title()} averaged {ppg} ppg over his career."
         else:
             avgs_szn = soup.find("tr", id=f"per_game.{self.season.split('-')[1]}")
             ppg = avgs_szn.find("td", {"data-stat": "pts_per_g"}).text
-            return f"{player_string.title()} averaged {ppg} ppg in the {self.season} season."
+            return f"{self.player.title()} averaged {ppg} ppg in the {self.season} season."
 
-    #NEED TO MOVE ENTIRE COMMAND TO A PREFIX COMMAND!
-    """
     @app_commands.command(name="ppg", description="Returns a player's ppg")
     async def pts_pg(self, interaction: discord.Interaction, player: str, draft_order: str, season : str="null"):
         self.player = player
@@ -67,22 +58,7 @@ class BbrefCommands(commands.Cog):
             plr_str = self.convert_name(self.player, self.draft_order)
             data = await self.page_tasks(session, f"https://basketball-reference.com/players/{plr_str[0]}/{plr_str}")
         msg = self.parse(data)
-        print(msg)
         await interaction.response.send_message(msg)
-    """
-
-    @commands.command(aliases=["ppg"])
-    async def pts_ppg(self, ctx, player: str, draft_order: str, season: str="null"):
-        self.player = player
-        self.draft_order = draft_order
-        self.season = season
-        async with aiohttp.ClientSession() as session:
-            plr_str = self.convert_name(self.player, self.draft_order)
-            data = await self.page_tasks(session, f"https://basketball-reference.com/players/{plr_str[0]}/{plr_str}")
-        msg = self.parse(data)
-        print(msg)
-        await ctx.send(msg)
-       
 
         
 
